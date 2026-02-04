9daaa34e8f685c1f610468973ef8e3e4736bc7d7
Hymsoft <hymsoft@gmail.com>
2026-02-03 23:03:57 -0300

    feat(legal): política de privacidad y términos de uso

--- src/constants/BuildVersion.ts
+++ src/constants/BuildVersion.ts
@@ -1,3 +1,3 @@
 // Este archivo es generado automáticamente por scripts/set-version.js
-export const BuildVersion = "1.0.0-build.0302262148";
-export const BuildDate = "2026-02-04T00:48:47.871Z";
+export const BuildVersion = "1.0.0-build.0302262243";
+export const BuildDate = "2026-02-04T01:43:16.573Z";
